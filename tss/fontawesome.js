1bcb43643dab5ef4b76e351a66eadb2ab3ba57e2
Cesar Estrada <cesar@codigomovil.mx>
2021-01-18 11:00:25 -0600

    update Font Awesome Free 5.15.2

--- tss/fontawesome.js
+++ tss/fontawesome.js
@@ -1,20 +1,20 @@
 /**
-* Font Awesome Free 5.15.1 by @fontawesome - https://fontawesome.com
+* Font Awesome Free 5.15.2 by @fontawesome - https://fontawesome.com
 * License - https://fontawesome.com/license/free (Icons: CC BY 4.0, Fonts: SIL OFL 1.1, Code: MIT License)
 */
 
 function getIcon(selector) {
-	return fontawesome[selector];
+	return fontawesome[ selector ];
 }
 exports.getIcon = getIcon;
 
 function setTitle(selector, object) {
-	object.title = fontawesome[selector];
+	object.title = fontawesome[ selector ];
 }
 exports.setTitle = setTitle;
 
 function setText(selector, object) {
-	object.text = fontawesome[selector];
+	object.text = fontawesome[ selector ];
 }
 exports.setText = setText;
 
