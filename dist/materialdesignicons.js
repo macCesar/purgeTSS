d961e34f2a9b73368702f4674cca11b458fadda4
Cesar Estrada <cesar@codigomovil.mx>
2021-11-08 11:35:54 -0600

    Code formatting

--- dist/materialdesignicons.js
+++ dist/materialdesignicons.js
@@ -8,25 +8,25 @@
 
 exports.getIcon = function getIcon(selector) {
 	if (selector === undefined) throw new Error('Selector missing!');
-	return (icons[ selector ] !== undefined) ? icons[ selector ] : fontawesome[ selector ];
+	return (icons[selector] !== undefined) ? icons[selector] : fontawesome[selector];
 };
 
 exports.setTitle = function setTitle(selector, object) {
 	if (selector === undefined || object === undefined) throw new Error('Selector or target Object missing!');
-	object.title = (icons[ selector ] !== undefined) ? icons[ selector ] : fontawesome[ selector ];
+	object.title = (icons[selector] !== undefined) ? icons[selector] : fontawesome[selector];
 };
 
 exports.setText = function setText(selector, object) {
 	if (selector === undefined || object === undefined) throw new Error('Selector or target Object missing!');
-	object.text = (icons[ selector ] !== undefined) ? icons[ selector ] : fontawesome[ selector ];
+	object.text = (icons[selector] !== undefined) ? icons[selector] : fontawesome[selector];
 };
 
 exports.getRandomKey = function getRandomKey() {
-	return Object.keys(icons)[ Math.floor(Math.random() * Object.keys(icons).length) ];
+	return Object.keys(icons)[Math.floor(Math.random() * Object.keys(icons).length)];
 };
 
 exports.getRandomValue = function getRandomValue() {
-	return icons[ Object.keys(icons)[ Math.floor(Math.random() * Object.keys(icons).length) ] ];
+	return icons[Object.keys(icons)[Math.floor(Math.random() * Object.keys(icons).length)]];
 };
 
 const icons = {
