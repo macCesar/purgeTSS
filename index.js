aeb1d800dab2e72f2495c92de2e70d8ff3940fcd
Cesar Estrada <cesar@codigomovil.mx>
2022-09-02 18:19:29 -0500

    Fix glossary generator

--- index.js
+++ index.js
@@ -1499,42 +1499,30 @@ function buildCustomTailwind(message = 'file created!') {
 
 	tailwindStyles += '\n// Resets\n';
 
+	//! Generate glossary files
 	let distributionFolder = !fs.existsSync(projectsConfigJS);
-	let destinationFolder = path.resolve(__dirname, './dist/glossary/');
-
 	if (distributionFolder) {
+		let destinationFolder = path.resolve(__dirname, './dist/glossary/');
 		makeSureFolderExists(destinationFolder);
-	}
 
-	let menuPosition = 1;
-
-	if (fs.existsSync(projectsConfigJS)) {
-		makeSureFolderExists(cwd + `/purgetss/tailwind`);
-	} else {
-		//
-	}
-	_.each(allValuesCombined, (value, key) => {
-		if (key.includes('Properties') && distributionFolder) {
-			destinationFolder = path.resolve(__dirname, './dist/glossary/' + key);
-			makeSureFolderExists(destinationFolder);
-			fs.writeFileSync(destinationFolder + '/_category_.json', `{ "label": "${key}", "position": ${menuPosition} }`);
-			menuPosition++;
-		} else {
-			let theClasses = helperToBuildCustomTailwindClasses(key, value);
-
-			if (destinationFolder) {
-				// fs.writeFileSync(`${destinationFolder}/${key}.md`, '```scss' + theClasses + '```');
-			}
-
-			//
-			if (fs.existsSync(projectsConfigJS)) {
-				saveFile(cwd + `/purgetss/tailwind/${key}.tss`, theClasses);
+		let menuPosition = 1;
+		_.each(allValuesCombined, (value, key) => {
+			if (key.includes('Properties')) {
+				destinationFolder = path.resolve(__dirname, './dist/glossary/' + key);
+				makeSureFolderExists(destinationFolder);
+				fs.writeFileSync(destinationFolder + '/_category_.json', `{ "label": "${key}", "position": ${menuPosition} }`);
+				menuPosition++;
 			} else {
-				//
+				let theClasses = helperToBuildCustomTailwindClasses(key, value);
+
+				if (destinationFolder) {
+					fs.writeFileSync(`${destinationFolder}/${key}.md`, '```scss' + theClasses + '```');
+				}
+
+				tailwindStyles += theClasses;
 			}
-			tailwindStyles += theClasses;
-		}
-	});
+		});
+	}
 
 	//! Compile @apply properties
 	let finalTailwindStyles = helpers.compileApplyDirectives(tailwindStyles);
