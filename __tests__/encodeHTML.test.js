b3cfdcfe4998f5a4df0bb527c8e2cf5cdba0b018
Cesar Estrada <cesar@codigomovil.mx>
2024-09-17 14:32:53 -0500

    remove temp folder

--- __tests__/encodeHTML.test.js
+++
@@ -1,7 +0,0 @@
-const { encodeHTML } = require('../index') // Asegúrate de que la ruta sea correcta
-
-test('encodeHTML should convert & to &amp;', () => {
-  const input = 'Hello & World'
-  const expectedOutput = 'Hello &amp; World'
-  expect(encodeHTML(input)).toBe(expectedOutput)
-})
