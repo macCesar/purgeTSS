8f9d28b4afb331f98d7d2eebd47640a6f7c1e63c
Cesar Estrada <cesar@codigomovil.mx>
2025-02-11 12:04:28 -0600

    v6.3.10

--- dist/purgetss.ui.js
+++ dist/purgetss.ui.js
@@ -1,4 +1,4 @@
-// Purge TSS v6.3.9
+// Purge TSS v6.3.10
 // Created by César Estrada
 // https://purgetss.com
 
@@ -169,7 +169,7 @@ function Animation(args = {}) {
 
     if (params.isIOS) {
       const { x, y } = calculateTranslation(draggableView, draggableView.parent.rect, left, top)
-      draggableView.applyProperties({ duration: 0, transform: Ti.UI.createMatrix2D().translate(x, y) });
+      draggableView.applyProperties({ duration: 0, transform: Ti.UI.createMatrix2D().translate(x, y) })
     } else {
       draggableView.animate(Ti.UI.createAnimation({ top, left, duration: 0 }))
     }
