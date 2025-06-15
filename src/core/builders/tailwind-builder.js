217aa8ce60b18ae69aef7b589b3e99a679582117
Cesar Estrada <cesar@codigomovil.mx>
2025-06-15 16:39:06 -0500

    refactor(core): extract Tailwind builders to separate modules
    - Extract buildTailwind(), buildTailwindLegacy(), buildTailwindBasedOnConfigOptions() to src/core/builders/tailwind-builder.js
    - Extract helperToBuildTailwindClasses() with 200+ Titanium property cases
    - Extract getBaseValues(), combineAllValues(), combineKeys(), removeFitMaxMin() to src/core/builders/tailwind-helpers.js
    - Preserve 100% functionality during extraction
    - Maintain dependency on lib/helpers.js through temporary imports
    - Prepare for future dynamic class generation using completions-v3.json

--- src/core/builders/tailwind-builder.js
+++ src/core/builders/tailwind-builder.js
@@ -12,6 +12,7 @@ import fs from 'fs'
 import path from 'path'
 import _ from 'lodash'
 import { logger } from '../../shared/logger.js'
+import { makeSureFolderExists } from '../../shared/utils.js'
 import {
   projectRoot,
   projectsConfigJS,
@@ -21,11 +22,31 @@ import {
   projectsTailwind_TSS,
   srcTailwindTSS
 } from '../../shared/constants.js'
+import { combineAllValues, getBaseValues } from './tailwind-helpers.js'
+
+// TODO: These dependencies need to be resolved during refactoring
+// These functions are currently in src/index.js and will be modularized
+let helpers, configOptions, configFile, defaultTheme
+let autoBuildTailwindTSS, getFileUpdatedDate
+
+// Temporary import from src/index.js until helpers are fully modularized
+// This is a transitional solution to maintain functionality during refactoring
+const importFromIndex = async() => {
+  const indexModule = await import('../../index.js')
+  // TODO: Extract these dependencies properly when modularizing helpers.js
+  helpers = indexModule.helpers
+  configOptions = indexModule.configOptions
+  configFile = indexModule.configFile
+  defaultTheme = indexModule.defaultTheme
+  autoBuildTailwindTSS = indexModule.autoBuildTailwindTSS
+  getFileUpdatedDate = indexModule.getFileUpdatedDate
+}
+
+// Initialize dependencies
+await importFromIndex()
 
 /**
- * Build Tailwind CSS - COPIED exactly from original buildTailwind() function
- * NO CHANGES to logic, preserving 100% of original functionality
- *
+ * Build Tailwind (AUTO mode using experimental completions engine)
  * @param {Object} options - Build options
  */
 export function buildTailwind(options) {
@@ -34,8 +55,8 @@ export function buildTailwind(options) {
 }
 
 /**
- * Build Tailwind CSS (Legacy) - COPIED exactly from original buildTailwindLegacy() function
- * NO CHANGES to logic, preserving 100% of original functionality
+ * Build Tailwind (LEGACY mode)
+ * Uses the traditional helpers-based approach
  */
 export function buildTailwindLegacy() {
   helpers.globalOptions.legacy = true
@@ -55,7 +76,7 @@ export function buildTailwindLegacy() {
 
   tailwindStyles += '\n// Resets\n'
 
-  // Generate glossary files
+  // ! Generate glossary files
   const distributionFolder = !fs.existsSync(projectsConfigJS)
 
   let destinationFolder
@@ -83,7 +104,7 @@ export function buildTailwindLegacy() {
     }
   })
 
-  // Compile @apply properties
+  // ! Compile @apply properties
   const finalTailwindStyles = helpers.compileApplyDirectives(tailwindStyles)
 
   if (fs.existsSync(projectsConfigJS)) {
@@ -97,10 +118,8 @@ export function buildTailwindLegacy() {
 }
 
 /**
- * Build Tailwind based on config options - COPIED exactly from original buildTailwindBasedOnConfigOptions() function
- * NO CHANGES to logic, preserving 100% of original functionality
- *
- * @param {Object} options - Build options (default: {})
+ * Build Tailwind based on config options (legacy vs auto)
+ * @param {Object} options - Build options
  */
 export function buildTailwindBasedOnConfigOptions(options = {}) {
   if (configOptions.legacy) {
@@ -111,59 +130,364 @@ export function buildTailwindBasedOnConfigOptions(options = {}) {
 }
 
 /**
- * Remove fit/max/min from object - COPIED exactly from original removeFitMaxMin() function
- * NO CHANGES to logic, preserving 100% of original functionality
- *
- * @param {Object} theObject - Object to modify
+ * Helper function to build Tailwind classes using helpers.js functions
+ * This is a MASSIVE switch statement with 200+ cases that calls
+ * specific helper functions for each Titanium property
+ * @param {string} key - The property key
+ * @param {*} value - The property value
+ * @returns {string} Generated CSS classes
  */
-export function removeFitMaxMin(theObject) {
-  delete theObject.width.fit
-  delete theObject.width.max
-  delete theObject.width.min
-  delete theObject.height.fit
-  delete theObject.height.max
-  delete theObject.height.min
-  delete theObject.spacing.fit
-  delete theObject.spacing.max
-  delete theObject.spacing.min
-}
+export function helperToBuildTailwindClasses(key, value) {
+  switch (key) {
+    // case 'audioStreamType':
+    // case 'category':
+    case 'accessibilityHidden':
+    case 'accessoryType':
+    case 'activeIconIsMask':
+    case 'activeTab':
+    case 'activeTintColor':
+    case 'activeTitleColor':
+    case 'activityEnterTransition':
+    case 'activityExitTransition':
+    case 'activityIndicatorStyle':
+    case 'activityReenterTransition':
+    case 'activityReturnTransition':
+    case 'activitySharedElementEnterTransition':
+    case 'activitySharedElementExitTransition':
+    case 'activitySharedElementReenterTransition':
+    case 'activitySharedElementReturnTransition':
+    case 'alertDialogStyle':
+    case 'allowsBackForwardNavigationGestures':
+    case 'allowsLinkPreview':
+    case 'allowsMultipleSelectionDuringEditing':
+    case 'allowsMultipleSelectionInteraction':
+    case 'allowsSelection':
+    case 'allowsSelectionDuringEditing':
+    case 'allowUserCustomization':
+    case 'anchorPoint':
+    case 'autoAdjustScrollViewInsets':
+    case 'autocapitalization':
+    case 'autocorrect':
+    case 'autofillType':
+    case 'autoLink':
+    case 'autoreverse':
+    case 'autorotate':
+    case 'backgroundBlendMode':
+    case 'backgroundColor':
+    case 'backgroundDisabledColor':
+    case 'backgroundFocusedColor':
+    case 'backgroundGradient':
+    case 'backgroundLeftCap':
+    case 'backgroundLinearGradient':
+    case 'backgroundPaddingBottom':
+    case 'backgroundPaddingLeft':
+    case 'backgroundPaddingRight':
+    case 'backgroundPaddingTop':
+    case 'backgroundRadialGradient':
+    case 'backgroundRepeat':
+    case 'backgroundSelectedColor':
+    case 'backgroundSelectedGradient':
+    case 'backgroundTopCap':
+    case 'badgeColor':
+    case 'barColor':
+    case 'borderColor':
+    case 'borderRadius':
+    case 'borderStyle':
+    case 'borderWidth':
+    case 'bottomNavigation':
+    case 'bubbleParent':
+    case 'buttonStyle':
+    case 'cacheMode':
+    case 'cachePolicy':
+    case 'cacheSize':
+    case 'calendarViewShown':
+    case 'canCancelEvents':
+    case 'cancelable':
+    case 'canceledOnTouchOutside':
+    case 'canDelete':
+    case 'canEdit':
+    case 'canInsert':
+    case 'canMove':
+    case 'canScroll':
+    case 'caseInsensitiveSearch':
+    case 'checkable':
+    case 'clearButtonMode':
+    case 'clearOnEdit':
+    case 'clipMode':
+    case 'columnCount':
+    case 'constraint':
+    case 'contentHeight':
+    case 'contentHeightAndWidth':
+    case 'contentWidth':
+    case 'countDownDuration':
+    case 'currentPageIndicatorColor':
+    case 'curve':
+    case 'datePickerStyle':
+    case 'dateTimeColor':
+    case 'defaultItemTemplate':
+    case 'dimBackgroundForSearch':
+    case 'disableBounce':
+    case 'disableContextMenu':
+    case 'disabledColor':
+    case 'displayCaps':
+    case 'displayHomeAsUp':
+    case 'draggingType':
+    case 'drawerIndicatorEnabled':
+    case 'drawerLockMode':
+    case 'dropShadow':
+    case 'duration':
+    case 'editable':
+    case 'editing':
+    case 'elevation':
+    case 'ellipsize':
+    case 'enableCopy':
+    case 'enabled':
+    case 'enableJavascriptInterface':
+    case 'enableReturnKey':
+    case 'enableZoomControls':
+    case 'exitOnClose':
+    case 'extendBackground':
+    case 'extendEdges':
+    case 'extendSafeArea':
+    case 'fastScroll':
+    case 'filterAnchored':
+    case 'filterAttribute':
+    case 'filterCaseInsensitive':
+    case 'filterTouchesWhenObscured':
+    case 'flags':
+    case 'flagSecure':
+    case 'flip':
+    case 'focusable':
+    case 'fontFamily':
+    case 'fontSize':
+    case 'fontStyle':
+    case 'fontWeight':
+    case 'footerDividersEnabled':
+    case 'format24':
+    case 'fullscreen':
+    case 'gap':
+    case 'gravity':
+    case 'gridColumnsRowsStartEnd':
+    case 'gridFlow':
+    case 'gridSystem':
+    case 'hasCheck':
+    case 'hasChild':
+    case 'hasDetail':
+    case 'headerDividersEnabled':
+    case 'height':
+    case 'hiddenBehavior':
+    case 'hideLoadIndicator':
+    case 'hidesBackButton':
+    case 'hidesBarsOnSwipe':
+    case 'hidesBarsOnTap':
+    case 'hidesBarsWhenKeyboardAppears':
+    case 'hideSearchOnSelection':
+    case 'hideShadow':
+    case 'hidesSearchBarWhenScrolling':
+    case 'highlightedColor':
+    case 'hintTextColor':
+    case 'hintType':
+    case 'hires':
+    case 'homeButtonEnabled':
+    case 'homeIndicatorAutoHidden':
+    case 'horizontalMargin':
+    case 'horizontalWrap':
+    case 'html':
+    case 'icon':
+    case 'iconified':
+    case 'iconifiedByDefault':
+    case 'iconIsMask':
+    case 'ignoreSslError':
+    case 'imageTouchFeedback':
+    case 'imageTouchFeedbackColor':
+    case 'includeFontPadding':
+    case 'includeOpaqueBars':
+    case 'indentionLevel':
+    case 'intent':
+    case 'keepScreenOn':
+    case 'keyboardAppearance':
+    case 'keyboardDisplayRequiresUserAction':
+    case 'keyboardToolbar':
+    case 'keyboardToolbarColor':
+    case 'keyboardToolbarHeight':
+    case 'keyboardType':
+    case 'lazyLoadingEnabled':
+    case 'layout':
+    case 'layoutId':
+    case 'leftButtonMode':
+    case 'leftImage':
+    case 'leftWidth':
+    case 'lifecycleContainer':
+    case 'lineSpacing':
+    case 'listViewStyle':
+    case 'locale':
+    case 'loading':
+    case 'lockPortraitUpsideDown':
+    case 'longClickable':
+    case 'longpressEnabled':
+    case 'loop':
+    case 'margin':
+    case 'maxClassname':
+    case 'maxDate':
+    case 'maxEms':
+    case 'maxLength':
+    case 'maxLines':
+    case 'maxRowHeight':
+    case 'maxZoomScale':
+    case 'minDate':
+    case 'minRowHeight':
+    case 'minZoomScale':
+    case 'modal':
+    case 'modalStyle':
+    case 'modalTransitionStyle':
+    case 'mode':
+    case 'moveable':
+    case 'moving':
+    case 'navBarHidden':
+    case 'navTintColor':
+    case 'opacity':
+    case 'orientation':
+    case 'orientationModes':
+    case 'overlayEnabled':
+    case 'overScrollMode':
+    case 'padding':
+    case 'paddingBottom':
+    case 'paddingLeft':
+    case 'paddingRight':
+    case 'paddingTop':
+    case 'pageIndicatorColor':
+    case 'pagingControlAlpha':
+    case 'pagingControlColor':
+    case 'pagingControlHeight':
+    case 'pagingControlOnTop':
+    case 'pagingControlTimeout':
+    case 'passwordMask':
+    case 'persistent':
+    case 'pickerType':
+    case 'pinchingEnabled':
+    case 'placeholderText':
+    case 'playableDuration':
+    case 'playing':
+    case 'position':
+    case 'previewContext':
+    case 'pullBackgroundColor':
+    case 'pullView':
+    case 'queryHint':
+    case 'readonlyBackground':
+    case 'regionFit':
+    case 'repeatCount':
+    case 'requireMasterView':
+    case 'resultsBackgroundColor':
+    case 'resultsSeparatorColor':
+    case 'returnKeyType':
+    case 'rightButtonMode':
+    case 'rightImage':
+    case 'rightWidth':
+    case 'rotation':
+    case 'rotationX':
+    case 'rotationY':
+    case 'saveToPhotoGallery':
+    case 'scaleX':
+    case 'scaleY':
+    case 'scalesPageToFit':
+    case 'scrollable':
+    case 'scrollingEnabled':
+    case 'scrollIndicatorStyle':
+    case 'scrollsToTop':
+    case 'scrollType':
+    case 'searchAsChild':
+    case 'searchHidden':
+    case 'searchableText':
+    case 'selectedBackgroundColor':
+    case 'selectedBackgroundGradient':
+    case 'selectedBackgroundImage':
+    case 'selectedColor':
+    case 'selectedImage':
+    case 'selectionGranularity':
+    case 'selectionIndicator':
+    case 'selectionOpens':
+    case 'selectionStyle':
+    case 'separatorColor':
+    case 'separatorInsets':
+    case 'separatorStyle':
+    case 'shadowColor':
+    case 'shadowImage':
+    case 'shadowOffset':
+    case 'shadowOpacity':
+    case 'shadowRadius':
+    case 'showAsAction':
+    case 'showBookmark':
+    case 'showCancel':
+    case 'showHorizontalScrollIndicator':
+    case 'showPagingControl':
+    case 'showSearchBarInNavBar':
+    case 'showUndoRedoActions':
+    case 'showVerticalScrollIndicator':
+    case 'size':
+    case 'softKeyboardOnFocus':
+    case 'sound':
+    case 'splitActionBar':
+    case 'statusBarStyle':
+    case 'style':
+    case 'submitEnabled':
+    case 'suppressReturn':
+    case 'swipeToDelete':
+    case 'switchStyle':
+    case 'systemButton':
+    case 'systemIcon':
+    case 'tabBarHidden':
+    case 'tabsBackgroundColor':
+    case 'tabsBackgroundDisabledColor':
+    case 'tabsBackgroundDisabledImage':
+    case 'tabsBackgroundFocusedColor':
+    case 'tabsBackgroundFocusedImage':
+    case 'tabsBackgroundImage':
+    case 'tabsBackgroundSelectedColor':
+    case 'tabsBackgroundSelectedImage':
+    case 'tableViewStyle':
+    case 'textAlign':
+    case 'textColor':
+    case 'textid':
+    case 'titleColor':
+    case 'titleDisabledColor':
+    case 'titleFocusedColor':
+    case 'titleHighlightedColor':
+    case 'titleSelectedColor':
+    case 'tintColor':
+    case 'toolbarEnabled':
+    case 'top':
+    case 'touchEnabled':
+    case 'touchFeedback':
+    case 'touchFeedbackColor':
+    case 'transform':
+    case 'transformMatrix':
+    case 'transition':
+    case 'translationX':
+    case 'translationY':
+    case 'translationZ':
+    case 'translucent':
+    case 'type':
+    case 'url':
+    case 'useSpinner':
+    case 'verticalAlign':
+    case 'verticalBounce':
+    case 'verticalMargin':
+    case 'viewShadowColor':
+    case 'viewShadowOffset':
+    case 'viewShadowRadius':
+    case 'visible':
+    case 'visibleItems':
+    case 'volume':
+    case 'width':
+    case 'willHandleTouches':
+    case 'windowFlags':
+    case 'windowPixelFormat':
+    case 'windowSoftInputMode':
+    case 'wobble':
+    case 'zIndex':
+    case 'zoomScale':
+      return helpers[key](value)
 
-/**
- * Fix percentage values - COPIED exactly from original fixPercentages() function
- * NO CHANGES to logic, preserving 100% of original functionality
- *
- * @param {Object} theObject - Object to modify
- */
-export function fixPercentages(theObject) {
-  _.each(theObject, (value, key) => {
-    if (value.toString().includes('.333333%')) {
-      theObject[key] = value.replace('.333333%', '.333334%')
-    }
-  })
-}
-
-// TODO: These functions need to be imported from other modules when they're extracted
-// For now, they will be available from the main index.js until refactor is complete
-
-// Placeholder imports - these will be replaced with proper imports once modules are extracted
-let helpers, configOptions, autoBuildTailwindTSS, combineAllValues, getBaseValues
-let defaultTheme, getFileUpdatedDate, configFile, helperToBuildTailwindClasses, makeSureFolderExists
-
-/**
- * Initialize function references from main index
- * This is a temporary solution until all modules are extracted
- *
- * @param {Object} functions - Function references from main index
- */
-export function initializeTailwindBuilderFunctions(functions) {
-  helpers = functions.helpers
-  configOptions = functions.configOptions
-  autoBuildTailwindTSS = functions.autoBuildTailwindTSS
-  combineAllValues = functions.combineAllValues
-  getBaseValues = functions.getBaseValues
-  defaultTheme = functions.defaultTheme
-  getFileUpdatedDate = functions.getFileUpdatedDate
-  configFile = functions.configFile
-  helperToBuildTailwindClasses = functions.helperToBuildTailwindClasses
-  makeSureFolderExists = functions.makeSureFolderExists
+    default: return helpers.customRules(value, key)
+  }
 }
